e1547c849af65aa65cac127902d2fb6a7c5f96c1
JeffZ07 <zhiwang260@gmail.com>
2021-02-04 19:28:35 +1300

    add new input data

---
+++ input.py
@@ -0,0 +1,138 @@
+import scipy.misc as sm
+import numpy as np
+import imageio
+import skimage.transform as trans
+import skimage.io as io
+from skimage.io import imsave, imread
+from matplotlib import pyplot as plt
+import cv2
+
+import os
+
+
+def load_hv_images(path):
+    paths = path.split(';')
+    root = "data/Jeff/rawinputs/"
+
+    image = io.imread(root+paths[0], as_gray=False)
+    # label = sm.imread(paths[1], mode='L')
+    column = io.imread(root+paths[2], as_gray=True)
+    wall = io.imread(root+paths[3], as_gray=True)
+    beam = io.imread(root+paths[4], as_gray=True)
+    grid = io.imread(root+paths[5], as_gray=True)
+
+    image = trans.resize(image, (512, 512, 3), mode='constant')
+
+    wall = trans.resize(wall, (512, 512))
+    column = trans.resize(column, (512, 512))
+    beam = trans.resize(beam, (512, 512))
+    grid = trans.resize(grid, (512, 512))
+
+    # wall = trans.resize(wall, (512, 512)) / 255.
+    # column = trans.resize(column, (512, 512)) / 255.
+    # beam = trans.resize(beam, (512, 512)) / 255.
+    # grid = trans.resize(grid, (512, 512)) / 255.
+
+    c_ind = (column < 0.5).astype(np.uint8)
+    w_ind = (wall < 0.5).astype(np.uint8)
+    b_ind = (beam < 0.5).astype(np.uint8)
+    g_ind = (grid < 0.5).astype(np.uint8)
+
+    label = np.zeros((512, 512))
+    label[c_ind == 1] = 1  # is column
+    label[w_ind == 1] = 2  # is wall
+    label[b_ind == 1] = 3  # is beam
+    label[g_ind == 1] = 4  # is gird
+
+    label = label.astype(np.uint8)
+    label = cv2.resize(label, (512, 512), interpolation=cv2.INTER_CUBIC)
+    img_new = np.asarray([label / 255.])
+    outdir = "data/Jeff/trainannot/"+paths[0]
+
+    imsave(outdir, label)
+    # cv2.imwrite(outdir, img_new)
+
+
+
+def LoadAllinOne():
+    root = "data/Jeff/rawinputs/"
+
+    files = os.listdir(root)
+
+    for file in files:
+        image = cv2.imread(root + file)
+        label = np.zeros((image.shape[0], image.shape[1]))
+        # image = trans.resize(image, (512, 512, 3), mode='constant')
+        for j in range(0, image.shape[0]):
+            for i in range(0, image.shape[1]):
+                item = image[j][i]
+                if isWall(item):
+                    label[j][i] = 2
+                elif isBeam(item):
+                    label[j][i] = 3
+                elif isColumn(item):
+                    label[j][i] = 1
+                else:
+                    label[j][i] = 0
+
+        merge = ind2rgb(label, color_map=plan_map)
+        plt.imshow(merge)
+        plt.show()
+        filestring = file.split()
+        outdir = "data/Jeff/trainannot/"+filestring[0] + " - label.png"
+        imsave(outdir, label)
+
+plan_map = {
+    0: [255, 255, 255],  # background
+    1: [165, 165, 0],  # column
+    2: [0, 165, 0],  # wall
+    3: [165, 0, 0],  # beam
+    4: [0, 0, 255],  # gird
+    5: [255, 160, 96],  # not used
+    6: [255, 224, 224],  # not used
+    7: [224, 224, 224],  # not used
+    8: [224, 224, 128]  # not used
+
+}
+
+def ind2rgb(ind_im, color_map):
+    rgb_im = np.zeros((ind_im.shape[0], ind_im.shape[1], 3))
+
+    for i, rgb in color_map.items():
+        rgb_im[(ind_im == i)] = rgb
+
+    return rgb_im
+
+
+def isWall(item):
+    aa = item[0]
+    bb = item[1]
+    cc = item[2]
+    #Lines
+    if (item[0] == 0 and item[1] == 166 and item[2] == 0):
+        return True
+    else:
+        return False
+
+def isBeam(item):
+    aa = item[0]
+    bb = item[1]
+    cc = item[2]
+    #Lines
+    if (item[0] == 250 and item[1] == 250 and item[2] == 0):
+        return True
+    else:
+        return False
+
+def isColumn(item):
+    aa = item[0]
+    bb = item[1]
+    cc = item[2]
+    #Lines
+    if (item[0] == 0 and item[1] == 250 and item[2] == 250):
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    LoadAllinOne()
--- train.py
+++ train.py
@@ -19,15 +19,15 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 if __name__ == '__main__':
 
     #path to images which are prepared to train a model
-    train_path = "CamVid"
+    train_path = "data/Jeff"
     image_folder = "train"
     label_folder = "trainannot"
-    valid_path =  "CamVid"
+    valid_path =  "data/Jeff"
     valid_image_folder ="val"
     valid_label_folder = "valannot"
     log_filepath = './log'
     flag_multi_class = True
-    num_classes = 12
+    num_classes = 4
     dp = data_preprocess(train_path=train_path,image_folder=image_folder,label_folder=label_folder,
                          valid_path=valid_path,valid_image_folder=valid_image_folder,valid_label_folder=valid_label_folder,
                          flag_multi_class=flag_multi_class,
@@ -40,10 +40,10 @@ if __name__ == '__main__':
     model = unet(num_class=num_classes)
 
     tb_cb = TensorBoard(log_dir=log_filepath)
-    model_checkpoint = keras.callbacks.ModelCheckpoint('./model/CamVid_model_v1.hdf5', monitor='val_loss',verbose=1,save_best_only=True)
+    model_checkpoint = keras.callbacks.ModelCheckpoint('./model/Jeff_model_v1.hdf5', monitor='val_loss',verbose=1,save_best_only=True)
     history = model.fit_generator(train_data,
-                                  steps_per_epoch=200,epochs=30,
-                                  validation_steps=10,
+                                  steps_per_epoch=5,epochs=1,
+                                  validation_steps=1,
                                   validation_data=valid_data,
                                   callbacks=[model_checkpoint,tb_cb])
 
